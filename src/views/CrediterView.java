1663ac467b3c13adb7d68d45af8e6ea9a58d02b3
Caroline Hilmoine <caro52130@gmail.com>
2023-01-13 16:09:00 +0100

    views 3

--- src/views/CrediterView.java
+++ src/views/CrediterView.java
@@ -1,29 +1,59 @@
 package views;
 import java.util.Scanner;
 
-import controllers.Operation;
+import javax.swing.JFrame;
 
-public class CrediterView {
+import controllers.Operation;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+
+public class CrediterView extends JFrame {
+	private JTextField MontantCredit;
+	public CrediterView() {
+		getContentPane().setLayout(null);
+		
+		JButton btnValiderCredit = new JButton("Valider");
+		btnValiderCredit.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		btnValiderCredit.setBounds(203, 289, 132, 35);
+		getContentPane().add(btnValiderCredit);
+		
+		MontantCredit = new JTextField();
+		MontantCredit.setHorizontalAlignment(SwingConstants.CENTER);
+		MontantCredit.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		MontantCredit.setBounds(155, 213, 232, 35);
+		getContentPane().add(MontantCredit);
+		MontantCredit.setColumns(10);
+		
+		JLabel lblMontantLabelC = new JLabel("Montant : ");
+		lblMontantLabelC.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMontantLabelC.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblMontantLabelC.setBounds(155, 167, 232, 35);
+		getContentPane().add(lblMontantLabelC);
+	}
 	
 	public static void main (String [] args) {
 		
-		Scanner sc =new Scanner(System.in);
-		
-		int numerocompte=123456;
-		double ancienSolde=10000.0;
-		
-		System.out.println("saisir le montant: ");
-		
-		double montant=sc.nextDouble();
-		Operation op =new Operation ();
-		
-	System.out.println("anciensolde="+ancienSolde+"nouveausolde="+op.crediter(numerocompte, montant,ancienSolde));
-		
+//		Scanner sc =new Scanner(System.in);
+//		
+//		int numerocompte=123456;
+//		double ancienSolde=10000.0;
+//		
+//		System.out.println("saisir le montant: ");
+//		
+//		double montant=sc.nextDouble();
+//		Operation op =new Operation ();
+//		
+//	System.out.println("anciensolde="+ancienSolde+"nouveausolde="+op.crediter(numerocompte, montant,ancienSolde));
+//		
 
 		
 		
 		
 	}
-	
-
 }
--- src/views/DebiterView.java
+++ src/views/DebiterView.java
@@ -5,8 +5,27 @@ import java.util.Scanner;
 import javax.swing.JFrame;
 
 import controllers.Operation;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 
 public class DebiterView extends JFrame {
+	private JTextField textField;
+	public DebiterView() {
+		getContentPane().setLayout(null);
+		
+		JLabel lblMontantDebit = new JLabel("Montant :");
+		lblMontantDebit.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMontantDebit.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblMontantDebit.setBounds(172, 160, 238, 55);
+		getContentPane().add(lblMontantDebit);
+		
+		textField = new JTextField();
+		textField.setBounds(170, 233, 240, 31);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+	}
 	public static void main (String [] args) {
 	
 	Scanner sc =new Scanner(System.in);
