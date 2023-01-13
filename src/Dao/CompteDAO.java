bd5b1a64063efe96fa86fb7e753fe0e22a4190fb
Caroline Hilmoine <caro52130@gmail.com>
2023-01-13 09:28:09 +0100

    User view

--- src/Dao/CompteDAO.java
+++ src/Dao/CompteDAO.java
@@ -1,5 +1,83 @@
 package Dao;
 
-public class CompteDAO {
+import java.sql.*;
+
+public class CompteDAO extends DatabaseConnection {
+	
+	Connection con;
+	
+	public void readCompte() {
+		Connection con = this.BDDconnection();
+
+		try {
+			java.sql.Statement stmt = con.createStatement();
+			ResultSet res = stmt.executeQuery("SELECT * FROM compte");
+			while (res.next()) {
+				System.out.println(res.getInt(1)+ " " +res.getString(2)+ " "+res.getString(3)+" "+res.getDouble(4)+" "+res.getInt(5)+" "+res.getString(6));
+			}
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void insertCompte() {
+		Connection con = this.BDDconnection();
+		int idcompte = 00007;
+		String nom = "Manmanu";
+		String prenom = "Valivali";
+		int solde = 30203;
+		int idbanque = 1;
+		String typecompte = "courant";
+		
+		try {
+			java.sql.Statement stmt = con.createStatement();
+			String sql = "INSERT INTO compte (idcompte, nom, prenom, solde,idbanque, typecompte) "
+					+ "VALUES ("+idcompte+", '"+nom+"', '"+prenom+"', "+solde+", "+idbanque+", '"+typecompte+"')";
+			stmt.executeUpdate(sql);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void deleteCompte() {
+		Connection con = this.BDDconnection();
+		int idCompte = 903874;
+		try {
+			java.sql.Statement stmt = con.createStatement();
+			String sql = "DELETE FROM compte WHERE idcompte="+idCompte+"";
+			stmt.executeUpdate(sql);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void updateCompte() {
+		Connection con = this.BDDconnection();
+		int idCompte = 847463;
+		double solde = 5;
+	
+		try {	
+			java.sql.Statement stmt = con.createStatement();
+			String sql = "UPDATE compte SET solde = "+solde+" WHERE idcompte = "+idCompte+"";
+			stmt.executeUpdate(sql);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+
+		CompteDAO compteBanque = new CompteDAO();
+		//compteBanque.readCompte();
+		//compteBanque.insertCompte();
+		//compteBanque.deleteCompte();
+		//compteBanque.updateCompte();
+	}
 
 }
--- src/views/UserView.java
+++ src/views/UserView.java
@@ -6,17 +6,38 @@ import java.awt.BorderLayout;
 import javax.swing.JButton;
 import java.awt.Color;
 import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class UserView extends JFrame {
-	private JTextField txtEntrerVotreNom;
+	
+	private JTextField Nom_Prenom;
+	
 	public UserView() {
 		
-		txtEntrerVotreNom = new JTextField();
-		txtEntrerVotreNom.setHorizontalAlignment(SwingConstants.CENTER);
-		txtEntrerVotreNom.setText("Entrer votre Nom et Prénom");
-		txtEntrerVotreNom.setBackground(new Color(128, 64, 64));
-		getContentPane().add(txtEntrerVotreNom, BorderLayout.CENTER);
-		txtEntrerVotreNom.setColumns(10);
+		getContentPane().setLayout(null);
+		
+		Nom_Prenom = new JTextField();
+		Nom_Prenom.setBounds(96, 218, 191, 20);
+		getContentPane().add(Nom_Prenom);
+		Nom_Prenom.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Entrez votre Nom et votre Prénom:");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(96, 196, 191, 14);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("MMCB BANQUE");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 32));
+		lblNewLabel_1.setBounds(49, 71, 278, 79);
+		getContentPane().add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Valider");
+		btnNewButton.setBounds(147, 249, 89, 23);
+		getContentPane().add(btnNewButton);
+		
+		}
 	}
 
-}
