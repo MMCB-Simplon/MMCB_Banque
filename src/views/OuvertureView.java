dfd55d897238cf6c7952c428b7c6002ccd933476
Caroline Hilmoine <caro52130@gmail.com>
2023-01-17 09:35:09 +0100

    update views

--- src/views/OuvertureView.java
+++ src/views/OuvertureView.java
@@ -1,5 +1,130 @@
 package views;
 
-public class OuvertureView {
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.ChangeListener;
 
+import java.awt.event.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+public class OuvertureView extends JFrame {
+	private JTextField textFieldNom;
+	private JTextField textFieldPrenom;
+	private JTextField textFieldSolde;
+	private JLabel lblNewLabelEpargne = new JLabel();
+	private JLabel lblNewLabelCourant = new JLabel();
+
+	public static void main(String[] args) throws Exception {
+		UIManager.setLookAndFeel(new NimbusLookAndFeel());
+		OuvertureView OuvertureView = new OuvertureView();
+		OuvertureView.setVisible(true);
+	}
+
+	public OuvertureView() {
+		super("Ouvrir un compte");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(1200, 650);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+
+		JPanel contentPane = (JPanel) this.getContentPane();
+		contentPane.setLayout(null);
+
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("SansSerif", Font.PLAIN, 22));
+		comboBox.setBounds(400, 130, 450, 40);
+		getContentPane().add(comboBox);
+
+		JRadioButton rdbtnRadioButtonEpargne = new JRadioButton("Compte Epargne");
+		rdbtnRadioButtonEpargne.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtnRadioButtonEpargne.setFont(new Font("SansSerif", Font.PLAIN, 22));
+		rdbtnRadioButtonEpargne.setBounds(167, 180, 220, 35);
+		getContentPane().add(rdbtnRadioButtonEpargne);
+
+		JRadioButton rdbtnRadioButtonCourant = new JRadioButton("Compte Courant");
+		rdbtnRadioButtonCourant.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtnRadioButtonCourant.setFont(new Font("SansSerif", Font.PLAIN, 22));
+		rdbtnRadioButtonCourant.setBounds(862, 180, 220, 35);
+		getContentPane().add(rdbtnRadioButtonCourant);
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(rdbtnRadioButtonEpargne);
+		group.add(rdbtnRadioButtonCourant);
+
+		JButton btnNewValiderButton = new JButton("Valider");
+		btnNewValiderButton.setFont(new Font("SansSerif", Font.PLAIN, 22));
+		btnNewValiderButton.setBounds(515, 467, 212, 41);
+		getContentPane().add(btnNewValiderButton);
+
+		textFieldNom = new JTextField();
+		textFieldNom.setToolTipText("Nom");
+		textFieldNom.setFont(new Font("SansSerif", Font.PLAIN, 22));
+		textFieldNom.setBounds(450, 239, 343, 41);
+		getContentPane().add(textFieldNom);
+		textFieldNom.setColumns(10);
+
+		textFieldPrenom = new JTextField();
+		textFieldPrenom.setToolTipText("Prénom");
+		textFieldPrenom.setFont(new Font("SansSerif", Font.PLAIN, 22));
+		textFieldPrenom.setColumns(10);
+		textFieldPrenom.setBounds(450, 314, 343, 41);
+		getContentPane().add(textFieldPrenom);
+
+		textFieldSolde = new JTextField();
+		textFieldSolde.setToolTipText("Solde initial");
+		textFieldSolde.setFont(new Font("SansSerif", Font.PLAIN, 22));
+		textFieldSolde.setColumns(10);
+		textFieldSolde.setBounds(450, 390, 343, 41);
+		getContentPane().add(textFieldSolde);
+
+		JLabel lblNewLabelNom = new JLabel("Nom");
+		lblNewLabelNom.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblNewLabelNom.setBounds(450, 213, 75, 26);
+		getContentPane().add(lblNewLabelNom);
+
+		JLabel lblNewLabelPrenom = new JLabel("Prénom");
+		lblNewLabelPrenom.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblNewLabelPrenom.setBounds(450, 289, 75, 26);
+		getContentPane().add(lblNewLabelPrenom);
+
+		JLabel lblNewLabelSolde = new JLabel("Solde initial");
+		lblNewLabelSolde.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblNewLabelSolde.setBounds(450, 367, 150, 26);
+		getContentPane().add(lblNewLabelSolde);
+
+		lblNewLabelEpargne.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabelEpargne.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		lblNewLabelEpargne.setBounds(121, 213, 299, 40);
+		getContentPane().add(lblNewLabelEpargne);
+
+		lblNewLabelCourant.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabelCourant.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		lblNewLabelCourant.setBounds(825, 213, 299, 40);
+		getContentPane().add(lblNewLabelCourant);
+
+//		lblNewLabelEpargne.addActionListener(new ActionListener());
+//		lblNewLabelCourant.addActionListener(new ActionListener());
+//
+//		@Override
+//		public actionPerformed(ActionEvent e) {
+//
+//			if (rdbtnRadioButtonEpargne.isSelected()) {
+		lblNewLabelEpargne.setText("Taux d'intérêt de 20%; Plafond de 10 000€");
+//					lblNewLabelEpargne.setVisible(true);
+//					lblNewLabelCourant.setVisible(false);			
+//
+//				else {
+//					if (rdbtnRadioButtonCourant.isSelected()) {			 
+		lblNewLabelCourant.setText("Frais de transfert de 2€, Solde minimum 0€");
+//				lblNewLabelCourant.setVisible(true);
+//				lblNewLabelEpargne.setVisible(false);			
+//				}
+//				}
+//			}
+//		}
+
+	}
 }
--- src/views/UserView.java
+++ src/views/UserView.java
@@ -1,45 +1,65 @@
 package views;
 
 import javax.swing.JFrame;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
+import java.awt.Dimension;
 import javax.swing.JTextField;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import java.awt.Color;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
 
 public class UserView extends JFrame {
-	
-	private JTextField Nom_Prenom;
-	
-	public UserView() {
-		
-		getContentPane().setLayout(null);
-		
-		Nom_Prenom = new JTextField();
-		Nom_Prenom.setBounds(198, 344, 402, 42);
-		getContentPane().add(Nom_Prenom);
-		Nom_Prenom.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Entrez votre Nom et votre Prénom:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(208, 294, 378, 35);
-		getContentPane().add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("MMCB BANQUE");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 37));
-		lblNewLabel_1.setBounds(198, 142, 378, 137);
-		getContentPane().add(lblNewLabel_1);
-		
-		JButton btnNewButton = new JButton("Valider");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 22));
-		btnNewButton.setBounds(338, 418, 148, 42);
-		getContentPane().add(btnNewButton);
-		
-		}
+
+	public static void main(String[] args) throws Exception {
+		UIManager.setLookAndFeel(new NimbusLookAndFeel());
+		UserView userView = new UserView();
+		userView.setVisible(true);
 	}
 
+	public UserView() {
+		super("Gestion bancaire");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(1200, 650);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+
+		JPanel contentPane = (JPanel) this.getContentPane();
+		contentPane.setLayout (null);
+		
+		JButton btnNewButton = new JButton("Valider");
+		btnNewButton.setFont(new Font("SansSerif", Font.PLAIN, 22));
+		btnNewButton.setBounds(500, 400, 200, 50);
+		getContentPane().add(btnNewButton);
+		
+		Nom_Prenom = new JTextField();
+		Nom_Prenom.setBounds(450, 300, 300, 50);
+		getContentPane().add(Nom_Prenom);
+
+		JLabel lblNewLabel = new JLabel("Entrez votre Nom et votre Prénom:");
+		lblNewLabel.setFont(new Font("SansSerif", Font.PLAIN, 22));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(410, 225, 378, 50);
+		getContentPane().add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("MMCB BANQUE");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("SansSerif", Font.BOLD, 37));
+		lblNewLabel_1.setBounds(410, 100, 378, 137);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("*Champ obligatoire: maximum 50 caractères");
+		lblNewLabel_2.setFont(new Font("SansSerif", Font.BOLD, 12));
+		lblNewLabel_2.setForeground(new Color(255, 0, 0));
+		lblNewLabel_2.setBounds(761, 317, 306, 16);
+		getContentPane().add(lblNewLabel_2);
+
+	}
+	private JTextField Nom_Prenom;
+}
